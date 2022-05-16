c9623b4ddf13ee459076e224424f19562e11ad34
izaakwalz <chukwumaizaak@gmail.com>
2022-05-16 14:15:49 +0100

    added: deployment scripts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,21 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
+const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
+
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  console.log("Deploying Ballot contract");
+  console.log("Proposals: ");
+  PROPOSALS.forEach((element, index) => {
+    console.log(`Proposal N. ${index + 1}: ${element}`);
+  });
+  const ballotFacctory = await ethers.getContractFactory("Ballot");
+  const ballot = await ballotFacctory.deploy(PROPOSALS);
+  await ballot.deployed();
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  // const text  = await  ballot.getText()
+  console.log("Ballot Proposal deployed to:", ballot.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
